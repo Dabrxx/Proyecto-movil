2f35ed9e58df1192e8429e937ddb667e4ce46d74
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2024-09-12 13:04:10 -0300

    Update environment.prod.ts
    Importe de firebase para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCb3PiH2NU_Pnm-NrZhBovJitb0QdpvKSo",
+    authDomain: "chilean-biodiversity.firebaseapp.com",
+    projectId: "chilean-biodiversity",
+    storageBucket: "chilean-biodiversity.appspot.com",
+    messagingSenderId: "1025729369272",
+    appId: "1:1025729369272:web:4e47ff0e43209b589d904d",
+    measurementId: "G-TGLG548VH1"
+  }
 };
