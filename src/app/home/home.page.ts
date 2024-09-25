2f84ce2a43911d89d61ed2761eb5ff6f8a917eeb
Unkname11 <to.torres@duocuc.cl>
2024-09-25 00:55:11 -0300

    Aplicadas las mejoras de TS de develop

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthenticaService } from '../authentica.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public route:Router ,public authService:AuthenticaService) {}
+
+  async logout(){
+    this.authService.signOut().then(()=>{
+      this.route.navigate(['/login'])
+    })
+  }
 
 }
