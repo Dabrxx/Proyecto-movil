a433d784a1467759aadb9f5355f2eedb01486561
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2024-10-16 14:29:42 -0300

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyCb3PiH2NU_Pnm-NrZhBovJitb0QdpvKSo",
-    authDomain: "chilean-biodiversity.firebaseapp.com",
-    projectId: "chilean-biodiversity",
-    storageBucket: "chilean-biodiversity.appspot.com",
-    messagingSenderId: "1025729369272",
-    appId: "1:1025729369272:web:4e47ff0e43209b589d904d",
-    measurementId: "G-TGLG548VH1"
+  	apiKey: "AIzaSyBG_25O92h-ZWRYCwI6NUhRdnBYYN5r4t4",
+  	authDomain: "ionicbiodiversity.firebaseapp.com",
+  	projectId: "ionicbiodiversity",
+  	storageBucket: "ionicbiodiversity.appspot.com",
+  	messagingSenderId: "500256860315",
+  	appId: "1:500256860315:web:f5653dfd7f20cac0a7fe3e"
   }
 };
