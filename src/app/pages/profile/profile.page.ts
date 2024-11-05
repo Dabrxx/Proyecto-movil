35b32f148c2e3a9ecc4f74f23654bd046aab11af
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2024-11-05 16:57:50 -0300

    Add files via upload

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -4,6 +4,8 @@ import { UserProfileService } from 'src/app/user-profile.service';
 import { Router } from '@angular/router';
 import { StorageService } from 'src/app/storageS.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-profile',
@@ -19,14 +21,14 @@ export class ProfilePage implements OnInit {
     private userProfileService: UserProfileService,
     private router: Router,
     private storageService: StorageService,
-    private ngFireAuth: AngularFireAuth
+    private ngFireAuth: AngularFireAuth,
+    private toastController: ToastController
   ) {}
 
   ngOnInit() {
     this.profileForm = this.formBuilder.group({
       displayName: ['',Validators.required],
       email: ['',[Validators.required, Validators.email]],
-      // para agregar otros campos
     })
     
     this.loadUserProfile()
@@ -40,34 +42,53 @@ export class ProfilePage implements OnInit {
       }
     } catch(error) {
       console.error('Error al cargar el perfil de usuario: ', error)
+      this.presentToast('Error al cargar el perfil de usuario.')
     }
   }
 
   async updateUserProfile() {
-    try{
-      const updateData = this.profileForm.value
-      const response = await this.userProfileService.updateUserProfile(updateData)
-      console.log(response.message)
-      alert(response.message)
+    try {
+       const emailInput = this.profileForm.get('email')?.value;
+       const user = await this.ngFireAuth.currentUser;
+ 
+       if (user && emailInput === user.email) {
+          const updateData = { displayName: this.profileForm.value.displayName };
+          const response = await this.userProfileService.updateUserProfile(updateData);
+          console.log(response.message);
+          alert(response.message);
+       } else {
+         this.presentToast('El correo ingresado no coincide con el correo de autenticación.');
+       }
     } catch (error) {
-      console.error('Error al actualizar el perfil: ', error)
+       console.error('Error al actualizar el perfil: ', error);
+       this.presentToast('Error al actualizar el perfil.')
     }
-  }
+ }
 
   async deletedUserProfile() {
     if(confirm('¿Estás seguro de que deseas eliminar tu cuenta? Esta acción es irreversible.')){
       try {
         const response = await this.userProfileService.deleteUserProfile()
         console.log(response.message)
-        alert(response.message)
+        this.presentToast(response.message)
 
         await this.storageService.clear();
         await this.ngFireAuth.signOut()
         this.router.navigate(['/landing'])
       } catch(error){
         console.error('Error al eliminar el perfil: ',error)
+        this.presentToast('Error al eliminar el perfil.')
       }
     }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000, // Duración en milisegundos
+      position: 'top'
+    });
+    toast.present();
   }
 
 
