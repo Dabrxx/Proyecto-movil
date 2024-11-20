6428e053271d3d9b82b7c75f9ee14b6960080940
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2024-11-20 16:45:04 -0300

    Update authentica.service.spec.ts

--- src/app/authentica.service.spec.ts
+++ src/app/authentica.service.spec.ts
@@ -1,16 +1,67 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
-import { AuthenticaService } from './authentica.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticaService {
 
-describe('AuthenticaService', () => {
-  let service: AuthenticaService;
+  constructor(
+    private ngFireAuth: AngularFireAuth,
+    private angularFireDatabase: AngularFireDatabase,
+  ) { }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthenticaService);
-  });
+  async registerUser(email: string, password: string, fullname: string): Promise<any> {
+    try {
+      // Registro del usuario en Firebase Authentication
+      const userCredential = await this.ngFireAuth.createUserWithEmailAndPassword(email, password);
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+      // Obtener el UID del usuario recién creado
+      const uid = userCredential.user?.uid;
+
+      if (uid) {
+        // Registrar al usuario en Firebase Realtime Database
+        await this.angularFireDatabase.object(`users/${uid}`).set({
+          displayName: fullname,
+          email: email,
+        });
+      }
+
+      return userCredential;
+    } catch (error) {
+      throw error; // Pasar el error al `SignUpUseCase` para manejarlo
+    }
+  }
+
+  async updateUserProfile(uid: string, displayName: string, email: string): Promise<void> {
+    try {
+      await this.angularFireDatabase.object(`users/${uid}`).update({
+        displayName: displayName,
+        email: email,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async loginUser (email:string, password:string) {
+    return await this.ngFireAuth.signInWithEmailAndPassword(email, password)
+  }
+
+  async resetPassword(email:string) {
+    return await this.ngFireAuth.sendPasswordResetEmail(email)
+  }
+
+  async signOut() {
+    return await this.ngFireAuth.signOut()
+  }
+
+  async getProfile() {
+    const user = await this.ngFireAuth.currentUser;
+    if (user && !user.displayName) {
+      console.warn('El displayName no está configurado en Firebase');
+    }
+    return user;
+  }
+}
