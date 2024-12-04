dc24667522c14c4cbdd72f7783f54edf7a5bc1c0
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2024-12-04 00:52:59 -0300

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-    firebaseConfig : {
+  firebaseConfig: {
     apiKey: "AIzaSyBG_25O92h-ZWRYCwI6NUhRdnBYYN5r4t4",
     authDomain: "ionicbiodiversity.firebaseapp.com",
     projectId: "ionicbiodiversity",
     storageBucket: "ionicbiodiversity.appspot.com",
     messagingSenderId: "500256860315",
     appId: "1:500256860315:web:f5653dfd7f20cac0a7fe3e"
+  },
+  mapboxAccessToken: 'pk.eyJ1IjoiYmlvZGl2ZXJzaWRhZCIsImEiOiJjbTQ2ZzJudmEwdGN1MnZwa3J5MWNmMjk4In0.u-3Ducr0OwlE-QsLYJmCiA', // Tu token de Mapbox
+  supabaseConfig: {
+    supabaseUrl: 'https://wijiomjwklxhqvvdwooc.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpamlvbWp3a2x4aHF2dmR3b29jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwOTcxNDAsImV4cCI6MjA0ODY3MzE0MH0.X21xUi4NU2p3kLdJgyoRcvOdsmP0RcjktodVG9p9NJg', // Reemplaza con tu clave
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
